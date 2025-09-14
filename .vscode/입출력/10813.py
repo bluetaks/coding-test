7935b52e76186e46d15e46b5098f2e567b00430a
bluetaks <jungtak99@gmail.com>
2025-09-14 22:42:57 +0900

    구현 문제 수정

--- .vscode/입출력/10813.py
+++ .vscode/입출력/10813.py
@@ -2,9 +2,6 @@ n,m = map(int,input().split())
 basket = [i for i in range(1,n+1)]
 for _ in range(m):
     i,j = map(int,input().split())
-    a = basket[i-1]
-    b = basket[j-1]
-    basket[j-1] = a
-    basket[i-1] = b
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
 for i in range(n):
     print(basket[i],end= " ")
