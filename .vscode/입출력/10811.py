d3a7ae707c83282307616dca8eab84b21be1a9c3
bluetaks <jungtak99@gmail.com>
2025-09-14 22:25:53 +0900

    역순 슬라이싱하고 적용시키기

---
+++ .vscode/입출력/10811.py
@@ -0,0 +1,10 @@
+n,m = map(int,input().split())
+numbers = [k for k in range(1,n+1)]
+temp = 0
+for _ in range(m):
+    i,j = map(int,input().split())
+    temp = numbers[i-1:j]
+    temp.reverse()
+    numbers[i-1:j] = temp
+print(numbers)
+    
