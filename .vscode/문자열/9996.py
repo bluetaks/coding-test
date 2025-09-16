075693e3a6b0a54fbc1bf43d3f2e1fa8625deb63
bluetaks <jungtak99@gmail.com>
2025-09-16 15:29:16 +0900

    앞과 뒤에 문자열로 시작과 끝나는지 알아보기

---
+++ .vscode/문자열/9996.py
@@ -0,0 +1,9 @@
+n = int(input())
+start , end = input().split("*")
+for _ in range(n):
+    text = input()
+    if len(start)+len(end) > len(text):
+        if text.startswith(start) and text.endswith(end):
+            print("DA")
+        else:
+            print("NE")
